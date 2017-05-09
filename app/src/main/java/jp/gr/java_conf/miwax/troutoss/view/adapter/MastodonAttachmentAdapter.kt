2f3b1e3dcfa72f137ac9a91c741f508a138f161e
Tomoya.Miwa <tomoyann@gmail.com>
2017-05-10 00:47:55 +0900

    Fix preview is not display.

--- app/src/main/java/jp/gr/java_conf/miwax/troutoss/view/adapter/MastodonAttachmentAdapter.kt
+++ app/src/main/java/jp/gr/java_conf/miwax/troutoss/view/adapter/MastodonAttachmentAdapter.kt
@@ -9,6 +9,8 @@ import com.sys1yagi.mastodon4j.api.entity.Attachment
 import jp.gr.java_conf.miwax.troutoss.R
 import jp.gr.java_conf.miwax.troutoss.databinding.RowAttachmentBinding
 import jp.gr.java_conf.miwax.troutoss.extension.imageUrl
+import timber.log.Timber
+import java.net.URI
 
 /**
  * Created by Tomoya Miwa on 2017/05/09.
@@ -25,7 +27,20 @@ class MastodonAttachmentAdapter(private val attachments: List<Attachment>) : Rec
     override fun getItemCount(): Int = attachments.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.previewImage?.imageUrl(attachments[position].previewUrl)
+        val attachment = attachments[position]
+        Timber.d("%d: type:%s, previewUrl:%s, url:%s, remote_url:%s",
+                position, attachment.type, attachment.previewUrl, attachment.url, attachment.remoteUrl)
+
+        // TODO: urlが画像以外だった場合の対処（固定の動画画像にするとか）
+        // TODO: 動画だった場合に再生マークを付ける
+        // TODO: 画像ロード中のプログレス表示検討
+        val url = when {
+            URI(attachment.previewUrl).isAbsolute -> attachment.previewUrl
+            URI(attachment.url).isAbsolute -> attachment.url
+            else -> attachment.remoteUrl
+        }
+
+        holder.binding.previewImage?.imageUrl(url)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
