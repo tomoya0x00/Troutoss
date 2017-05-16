b66133580ea06a887a554369c93e354ad043286c
Tomoya.Miwa <tomoyann@gmail.com>
2017-05-16 14:17:20 +0900

    Implement basic layout of PostStatusActivity.

--- app/src/main/java/jp/gr/java_conf/miwax/troutoss/view/activity/PostStatusActivity.kt
+++ app/src/main/java/jp/gr/java_conf/miwax/troutoss/view/activity/PostStatusActivity.kt
@@ -2,22 +2,41 @@ package jp.gr.java_conf.miwax.troutoss.view.activity
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import jp.gr.java_conf.miwax.troutoss.R
+import jp.gr.java_conf.miwax.troutoss.databinding.ActivityPostStatusBinding
 import jp.gr.java_conf.miwax.troutoss.model.entity.AccountType
 
 class PostStatusActivity : AppCompatActivity() {
 
-    val accountType: AccountType by lazy {
+    lateinit private var binding: ActivityPostStatusBinding
+
+    private val accountType: AccountType by lazy {
         intent.extras.getString(EXTRA_ACCOUNT_TYPE)?.let { AccountType.valueOf(it) } ?: AccountType.UNKNOWN
     }
 
-    val accountUuid: String by lazy { intent.extras.getString(EXTRA_ACCOUNT_UUID) }
+    private val accountUuid: String by lazy { intent.extras.getString(EXTRA_ACCOUNT_UUID) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_status)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_post_status)
+
+         supportActionBar?.let {
+            it.displayOptions = ActionBar.DISPLAY_SHOW_TITLE or ActionBar.DISPLAY_SHOW_HOME or ActionBar.DISPLAY_HOME_AS_UP
+            it.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return false
     }
 
     companion object {
