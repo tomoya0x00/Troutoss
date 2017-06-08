25df40b4854a4c69b16dfb6d04d2e75e8fbc8a55
Tomoya.Miwa <tomoyann@gmail.com>
2017-06-08 10:34:02 +0900

    Add Crashlytics.

--- app/src/main/java/jp/gr/java_conf/miwax/troutoss/App.kt
+++ app/src/main/java/jp/gr/java_conf/miwax/troutoss/App.kt
@@ -5,11 +5,13 @@ import android.content.Context
 import android.content.res.Resources
 import android.support.multidex.MultiDexApplication
 import android.util.Log
+import com.crashlytics.android.Crashlytics
 import com.deploygate.sdk.DeployGate
 import com.facebook.stetho.Stetho
 import com.google.firebase.crash.FirebaseCrash
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.uphyca.stetho_realm.RealmInspectorModulesProvider
+import io.fabric.sdk.android.Fabric
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import timber.log.Timber
@@ -35,6 +37,8 @@ class App : MultiDexApplication() {
         } else {
             Timber.plant(FirebaseTree())
         }
+
+        Fabric.with(this, Crashlytics())
 
         // Realmデフォルト設定
         Realm.init(this)
